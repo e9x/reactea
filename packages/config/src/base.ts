fdaa928dfcfc09ea6ee9db43d2a954d5c1025328
David Reed <reedswork@proton.me>
2023-02-25 20:45:04 -0500

    separate core

--- packages/config/src/base.ts
+++ packages/config/src/base.ts
@@ -1,19 +1,11 @@
 import "webpack-dev-server";
-import { appDir } from "./consts.js";
+import coreConfig from "./core.js";
 import cssConfig from "./css.js";
-import { envRaw, envRawStringified } from "./env.js";
+import { envRaw } from "./env.js";
 import htmlConfig from "./html.js";
 import { createConfig, extendConfig } from "./index.js";
 import jsConfig from "./js.js";
 import svgConfig from "./svg.js";
-import CaseSensitivePathsPlugin from "@umijs/case-sensitive-paths-webpack-plugin";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
-import ESLintWebpackPlugin from "eslint-webpack-plugin";
-import { join, resolve } from "node:path";
-import ModuleNotFoundPlugin from "react-dev-utils/ModuleNotFoundPlugin.js";
-import formatter from "react-dev-utils/eslintFormatter.js";
-import webpack from "webpack";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export const shouldLint = process.env.DISABLE_LINT !== "true";
 
@@ -41,53 +33,9 @@ export const isEnvProductionProfile =
 export const shouldUseReactRefresh = envRaw.FAST_REFRESH;
 
 export default function baseConfig() {
-  const config = createConfig({
-    plugins: [
-      // unused assets
-      new CleanWebpackPlugin(),
-      // This gives some necessary context to module not found errors, such as
-      // the requesting resource.
-      new ModuleNotFoundPlugin(resolve(".")),
-      // Makes some environment variables available to the JS code, for example:
-      // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
-      // It is absolutely essential that NODE_ENV is set to production
-      // during a production build.
-      // Otherwise React will be compiled in the very slow development mode.
-      new webpack.DefinePlugin({
-        "process.env": envRawStringified,
-      }),
-      // Watcher doesn't work well if you mistype casing in a path so we use
-      // a plugin that prints an error when you attempt to do this.
-      // See https://github.com/facebook/create-react-app/issues/240
-      ...(isDevelopment ? [new CaseSensitivePathsPlugin()] : []),
-      ...(process.env.WEBPACK_BUNDLE_ANALYZER === "true"
-        ? [
-            new BundleAnalyzerPlugin({
-              analyzerMode: "static",
-            }),
-          ]
-        : []),
-      ...(shouldLint
-        ? [
-            new ESLintWebpackPlugin({
-              // Plugin options
-              extensions: ["js", "mjs", "jsx", "ts", "tsx"],
-              formatter,
-              eslintPath: "eslint",
-              failOnError: !(isDevelopment && emitErrorsAsWarnings),
-              cache: true,
-              cacheLocation: join(
-                appDir,
-                "node_modules",
-                ".cache",
-                ".eslintcache"
-              ),
-            }),
-          ]
-        : []),
-    ],
-  });
+  const config = createConfig();
 
+  extendConfig(config, coreConfig());
   extendConfig(config, htmlConfig());
   extendConfig(config, jsConfig());
   extendConfig(config, svgConfig());
---
+++ packages/config/src/core.ts
@@ -0,0 +1,61 @@
+import { emitErrorsAsWarnings, isDevelopment, shouldLint } from "./base";
+import { appDir } from "./consts.js";
+import { envRawStringified } from "./env.js";
+import { createConfig } from "./index.js";
+import CaseSensitivePathsPlugin from "@umijs/case-sensitive-paths-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+import ESLintWebpackPlugin from "eslint-webpack-plugin";
+import { join, resolve } from "node:path";
+import ModuleNotFoundPlugin from "react-dev-utils/ModuleNotFoundPlugin.js";
+import formatter from "react-dev-utils/eslintFormatter.js";
+import webpack from "webpack";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
+
+export default function coreConfig() {
+  return createConfig({
+    plugins: [
+      // unused assets
+      new CleanWebpackPlugin(),
+      // This gives some necessary context to module not found errors, such as
+      // the requesting resource.
+      new ModuleNotFoundPlugin(resolve(".")),
+      // Makes some environment variables available to the JS code, for example:
+      // if (process.env.NODE_ENV === 'production') { ... }. See `./env.js`.
+      // It is absolutely essential that NODE_ENV is set to production
+      // during a production build.
+      // Otherwise React will be compiled in the very slow development mode.
+      new webpack.DefinePlugin({
+        "process.env": envRawStringified,
+      }),
+      // Watcher doesn't work well if you mistype casing in a path so we use
+      // a plugin that prints an error when you attempt to do this.
+      // See https://github.com/facebook/create-react-app/issues/240
+      ...(isDevelopment ? [new CaseSensitivePathsPlugin()] : []),
+      ...(process.env.WEBPACK_BUNDLE_ANALYZER === "true"
+        ? [
+            new BundleAnalyzerPlugin({
+              analyzerMode: "static",
+            }),
+          ]
+        : []),
+      ...(shouldLint
+        ? [
+            new ESLintWebpackPlugin({
+              // Plugin options
+              extensions: ["js", "mjs", "jsx", "ts", "tsx"],
+              formatter,
+              eslintPath: "eslint",
+              failOnError: !(isDevelopment && emitErrorsAsWarnings),
+              cache: true,
+              cacheLocation: join(
+                appDir,
+                "node_modules",
+                ".cache",
+                ".eslintcache"
+              ),
+            }),
+          ]
+        : []),
+    ],
+  });
+}
