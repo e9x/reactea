f7d7b7f5cc43d9ec016c64e141edf073ec117e52
David Reed <reedswork@proton.me>
2023-02-27 19:24:16 -0500

    check if isDevelopment

--- packages/sw/src/index.ts
+++ packages/sw/src/index.ts
@@ -1,6 +1,6 @@
 import type { ReacteaConfig } from "@reactea/config";
 import { createConfig } from "@reactea/config";
-import { resolveModule, srcDir } from "@reactea/config/consts";
+import { isDevelopment, resolveModule, srcDir } from "@reactea/config/consts";
 import { join } from "node:path";
 import { InjectManifest } from "workbox-webpack-plugin";
 
@@ -11,15 +11,19 @@ export default async function swConfig(): Promise<ReacteaConfig> {
 
   return createConfig({
     plugins: [
-      new InjectManifest({
-        swSrc,
-        dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
-        exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
-        // Bump up the default maximum size (2mb) that's precached,
-        // to make lazy-loading failure scenarios less likely.
-        // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
-        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
-      }),
+      ...(!isDevelopment
+        ? [
+            new InjectManifest({
+              swSrc,
+              dontCacheBustURLsMatching: /\.[0-9a-f]{8}\./,
+              exclude: [/\.map$/, /asset-manifest\.json$/, /LICENSE/],
+              // Bump up the default maximum size (2mb) that's precached,
+              // to make lazy-loading failure scenarios less likely.
+              // See https://github.com/cra-template/pwa/issues/13#issuecomment-722667270
+              maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+            }),
+          ]
+        : []),
     ],
   });
 }
